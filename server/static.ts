26bdb0474e4ba29df172f0f68bbb2cb8080a2058
Prince <chourasiya8919@gmail.com>
2026-01-17 22:55:23 +0530

    completed

--- server/static.ts
+++ server/static.ts
@@ -3,11 +3,20 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "client");
+  const distPath = path.resolve(__dirname, "public");
   if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
+    // a bit of a hack to make it work locally
+    const localPath = path.resolve(process.cwd(), "dist/public");
+    if (!fs.existsSync(localPath)) {
+      throw new Error(
+        `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      );
+    }
+    app.use(express.static(localPath));
+    app.use("/{*path}", (_req, res) => {
+      res.sendFile(path.resolve(localPath, "index.html"));
+    });
+    return;
   }
 
   app.use(express.static(distPath));
